214825ff1d5f70a61136654b828c204c0cea151a
shenzhuoyan <2598925230@qq.com>
2023-05-12 18:19:24 +0800

    前面几个忘了判断版本

--- preload.js
+++ preload.js
@@ -1,9 +1,12 @@
 const { exec } = require('child_process')
 const fs = require('fs');
+const confpath = `$HOME/.config/findlinux`
+const version = 2
 window.exec = function (keyword, callback) {
-    exec("~/.config/findlinux/find.sh "+keyword, (err, stdout, stderr) => {
+    exec(confpath+`/find.sh `+keyword, (err, stdout, stderr) => {
       if (err) {
-        console.log("执行错误")
+        console.log("无结果")
+        callback()//没结果返回个null
         return 0
       }
       callback(stdout)
@@ -17,31 +20,28 @@ window.exports = {
      args: {
         // 进入插件应用时调用（可选）
         enter: (action, callbackSetList) => {
-          //console.log("进入插件")
-          exec("if [ ! -f ~/.config/findlinux/find.sh ]; then echo yes; else echo no ;fi",(err, stdout, stderr)=>{
+          // console.log("进入插件")
+          // 注意更新时修改当前版本
+          // 判断是否已有配置（第一次安装）或版本是否需要升级
+          exec(`if [ ! -f `+confpath+`/version ] || (( `+version+` > $(cat $HOME/.config/findlinux/version) ));`+
+          `then echo yes; else echo no ;fi`,(err, stdout, stderr)=>{
             console.log(stdout)
-            if(stdout=="yes\n"){// 如果文件不存在就创建
+            if(stdout=="yes\n"){// 如果文件不存在或者版本比当前低就重新生成
               console.log("文件不存在")
-              exec("mkdir ~/.config/findlinux -p",(err, stdout, stderr)=>{})
-              exec('echo "#\!/bin/bash\n'+
-                    '# 判断命令是哪个\n'+
-                    'if hash fdfind 2>/dev/null; then\ncmd=\"fdfind\"\nelse cmd=\"fd-find\"\nfi\n'+
-                    '# 参数如下\n# --search-path 从用户家目录下搜索\n'+
-                    '# --max-results 60 只显示最多60条\n'+
-                    '# -F 字符串只当作文本而不是正则表达式\n'+
-                    '# -a 显示全路径\n'+
-                    '# 更多参数使用 man fdfind 或 man fd-find\n'+
-                    'result=\\`\\$cmd --search-path ~ -Fa \\$1\\`\n'+
-                    'shift\n'+
-                    'for arg in \\$*;do result=\\`echo \\"\\$result\\" | grep \\$arg\\`;done\n'+
-                    'count=0\n'+
-                    'echo \\"\\$result\\" | while read line;do\n'+
-                    'count=\\$((\\$count+1))\n'+
-                    'echo \\$line\n'+
-                    'if [ \\$count = 60 ];then\n'+
-                    'break\nfi\n'+
-                    'done" > ~/.config/findlinux/find.sh',(err, stdout, stderr)=>{})
-              exec('chmod +x ~/.config/findlinux/find.sh',(err, stdout, stderr)=>{})
+              exec(`mkdir -p `+ confpath,(err, stdout, stderr)=>{})
+              // 重新生成文件
+              exec(`echo "#\!/bin/bash\n# 判断命令是哪个\nif hash fdfind 2>/dev/null; then\ncmd=fdfind\nelse cmd=fd-find\nfi\n`+
+              `# 参数如下\n# --search-path 从用户家目录下搜索\n# --max-results 120 只显示最多120条\n`+
+              `# -F 字符串只当作文本而不是正则表达式\n# -a 显示全路径\n# 更多参数使用 man fdfind 或 man fd-find\n`+
+              `# 如果输入的不止1个字母\nif (( \\$\{#1\} > 1 ));then\nresult=\\$(\\$cmd --search-path ~ -Fa \\$1)\nshift\n`+
+              `for arg in \\$*;do result=\\$(echo \\"\\$result\\" | grep \\$arg);done\ncount=0\n`+
+              `echo \\"\\$result\\" | while read line;do\ncount=\\$((\\$count+1))\necho \\$line\n`+
+              `if [ \\$count = 120 ];then\nbreak\nfi\ndone\nelse\n# 只输入一个字母就进行精准匹配\n`+
+              `\\$cmd --search-path ~ -Fa \\$1 | grep \\"/\\$1\\$\\"\nfi" > `+confpath+`/find.sh`,(err, stdout, stderr)=>{})
+              // 记录本地版本
+              exec(`echo `+version+` > `+confpath+`/version`)
+              // 添加执行权限
+              exec('chmod +x '+confpath+`/find.sh`,(err, stdout, stderr)=>{})
             }
           })
         },
@@ -55,18 +55,21 @@ window.exports = {
            var arr = []
            if(searchWord!=""){//如果有输入，就搜索并展示结果
             window.exec(searchWord,function(content){
-                
-                
-                  for(var item of content.split("\n")){
-                    if(item!="")
-                      arr.push({
-                        title:item,
-                        description:fs.statSync(item).isFile()?"文件":"目录",
-                        url:item,
-                        isFile:fs.statSync(item).isFile()
-                      });
-                  }
-                  callbackSetList(arr) 
+              // console.log(content)
+              if(content!=null){//如果搜索结果是空，列表就置空
+                //解决了关键词没结果的时候列表还是上次结果的bug
+                for(var item of content.split("\n")){
+                  if(item!="")
+                    arr.push({
+                      title:item,
+                      description:fs.statSync(item).isFile()?"文件":"目录",
+                      url:item,
+                      isFile:fs.statSync(item).isFile()
+                    });
+                }
+              }
+                callbackSetList(arr) 
+              
               })
             }
             else {
@@ -77,7 +80,6 @@ window.exports = {
         },
         // 用户选择列表中某个条目时被调用
         select: (action, itemData, callbackSetList) => {
-            
             window.utools.hideMainWindow()
             // const url = itemData.url
             //require('electron').shell.openExternal(url)
